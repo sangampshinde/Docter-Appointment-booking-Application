1c1debc3eddd0011d85d398a27c40de112d169d0
sangam shinde <sangampshinde@gmail.com>
2024-06-14 21:53:42 +0530

    hashing password with becrypt pakage

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
-router.post("/register", async () => {
+router.post("/register", async (req,res) => {
   try {
+    const password = req.body.password;
+    
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(200).send({message: 'User saved successfully',success: true});
+    
+    
 
   } catch (error) {
 
