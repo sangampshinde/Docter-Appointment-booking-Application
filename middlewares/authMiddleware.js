d70dd8c4eee20d44d2a8b12e95a745cdf273018c
sangam shinde <sangampshinde@gmail.com>
2024-06-15 23:13:39 +0530

    extracting tocken from local storage

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,7 @@
 const jwt = require('jsonwebtoken');
+
+module.exports = (req,res,next)=>{
+// we here extrating the token innheader and spliit it out from string 
+    const token = req.headers["authorization"].split(" ")[1]
+    
+}
