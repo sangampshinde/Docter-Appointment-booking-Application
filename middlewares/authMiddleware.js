454c20b483cffa53e4f349eec0cca2726c097f6c
sangam shinde <sangampshinde@gmail.com>
2024-06-16 22:41:01 +0530

    module error resovr

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,23 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-module.exports = (req,res,next)=>{
-// we here extrating the token innheader and spliit it out from string 
-    const token = req.headers["authorization"].split(" ")[1]
-    
-}
+module.exports = async(req, res, next) => {
+  try {
+    // we here extrating the token innheader and spliit it out from string
+    const token = req.headers["authorization"].split(" ")[1];
+    // first parameter is the token and second parameter is the secret t
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ message: "unauthorized", success: false });
+      } else {
+        req.body.userId = decoded._id;
+        next();
+      }
+    });
+  } catch (error) {
+    res.status(401).send({ message: "unauthorized", success: false });
+
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/userModel');//
+const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post("/register", async (req,res) => {
+router.post("/register", async (req, res) => {
   try {
     const userExists = await User.findOne({ email: req.body.email });
-    if(userExists) {
-      return res.status(200).send({message: 'User already exists',success: false}); 
+    if (userExists) {
+      return res.status(200).send({ message: 'User already exists', success: false });
     }
 
     const password = req.body.password;
@@ -18,54 +19,54 @@ router.post("/register", async (req,res) => {
     req.body.password = hashedPassword;
     const newUser = new User(req.body);
     await newUser.save();
-    res.status(200).send({message: 'User saved successfully',success: true});
-    
-    
+    res.status(200).send({ message: 'User saved successfully', success: true });
 
   } catch (error) {
     console.log(error);
-     res.status(500).send({message:'error creating user',sucess:false,error});
+    res.status(500).send({ message: 'Error creating user', success: false, error });
 
   }
 });
 
-// login page logic 
-router.post("/login", async (req,res) => {
-   console.log(req.body.email);
+// Login logic
+router.post("/login", async (req, res) => {
+  console.log(req.body.email);
   try {
-    const user= await User.findOne({email: req.body.email});
-    // if user not found
-    if(!user) {
-      return res.status(200).send({message: 'User not found',success: false});
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(200).send({ message: 'User not found', success: false });
     }
-    // comparing becrypt password with user enter password  parameters are just like(normal password,database password)
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
 
-    if(!isMatch){
-      return res.status(200).send({message: 'Invalid password',success: false});
-    }else{
-      // generating token
-      // here we need to generate the token first parameter is payload and second parameter is secret key
-      // jwtsign is method to generate token
-      const token = jwt.sign({_id:user._id},process.env.TOKEN_SECRET,{
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(200).send({ message: 'Invalid password', success: false });
+    } else {
+      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
         expiresIn: "1h"
       });
-      res.status(200).send({message: 'User logged in successfully',success: true, data:token}); 
+      res.status(200).send({ message: 'User logged in successfully', success: true, data: token });
     }
 
   } catch (error) {
     console.log(error);
-    res.status(500).send({message: "error in login",success: false,error: error});
+    res.status(500).send({ message: "Error in login", success: false, error: error });
 
   }
 });
 
-router.post("get-user-info-by-id",async (req, res)=> {
+// Protected routes
+router.post("/get-user-info-by-id", authMiddleware, async (req, res) => {
   try {
-    
+    const user = await User.findOne({ _id: req.body.userId });
+    if (!user) {
+      return res.status(200).send({ message: 'User not found', success: false });
+    } else {
+      return res.status(200).send({ success: true, data: { user: user.name, email: user.email } });
+    }
   } catch (error) {
-    
-  }
+    res.status(500).send({ message: 'Error while getting user info', success: false, error });
 
-})
+  }
+});
+
 module.exports = router;
